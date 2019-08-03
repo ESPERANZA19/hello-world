dd49c86a7e5e4de91ecf7759cc5e5caf8254253c
ESPERANZA19 <53652376+ESPERANZA19@users.noreply.github.com>
2019-08-03 19:28:51 +0530

    index.php

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Momentz Photography</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body data-spy="scroll" data-offset="50">
+    <div class="bg">
+<div id="mySidenav">
+  <a href="#section1" id="Home">Home</a>
+  <a href="#section2" id="About">About</a>
+  <a href="#section3" id="Gallery">Gallery</a>
+  <a href="#section4" id="Contact">Contact</a></div>
+
+        <div id="section1" class="pa"> <h2><div class="head" >MOMENTUZ STUDIO &nbsp;&nbsp;</div> </h2><i id="cap"> "Today's little moments <br>
+        become tomorrow's precious memories.. "</i>                                                                                                
+</div>
+        <div id="section2" class="pa"><h1> SERVICES </h1><p>We consider it as the essence of 
+            communication as every moment has its own story, idea, and message.  
+            With this in mind, we strive to meet our customer’s needs enabling them of accomplishing their
+            goals and documenting their special moments.  Our Dubai-based studio is led by creative and innovative
+            team of professional photographers, videographers, with over 20 years of experience  and state-of-the-art
+            equipment and technologies. We cater to your need for, advertising products, corporate videos, wedding events & photography.  We will stand alongside you in the corporate world too, taking your professional portraits and working on the highest quality event,  with video production for your company and clients. 
+
+            </p><p>Momentus studio has the experience of catering to, IT, Healthcare, Hospitality, Education, Manufacturing and more industries Our team of passionate, creative photographers and videographers share the desire to make every experience with Momentuz Studio the best it can possibly be.  We welcome you to visit us and experience our services.</P></div>
+        <div id="section3" class="pa"><h1> GALLERY </h1><div class="gallery">
+  <a target="_blank" href="pp.jpg">
+    <img src="gg.jpg" alt="Cinque Terre" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="oo.jpg">
+    <img src="yy.jpg" alt="Forest" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+<div class="gallery">
+  <a target="_blank" href="pp.jpg">
+    <img src="gg.jpg" alt="Cinque Terre" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="oo.jpg">
+    <img src="yy.jpg" alt="Forest" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+<div class="gallery">
+  <a target="_blank" href="pp.jpg">
+    <img src="gg.jpg" alt="Cinque Terre" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="oo.jpg">
+    <img src="yy.jpg" alt="Forest" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+<div class="gallery">
+  <a target="_blank" href="pp.jpg">
+    <img src="gg.jpg" alt="Cinque Terre" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="oo.jpg">
+    <img src="yy.jpg" alt="Forest" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="gg.jpg">
+    <img src="gg.jpg" alt="Northern Lights" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="ww.jpg">
+    <img src="ww.jpg" alt="Mountains" width="600" height="400">
+  </a>
+  <div class="desc">Add a description of the image here</div>
+</div></div>
+        <div id="section4" class=""><h1>CONTACT US</h1>
+        <div class="row">
+                <div class="col-md-6">
+            <form class="my-form">
+                <div class="form-group">
+                    <label for="form-name">Name</label>
+                    <input type="email" class="form-control" id="form-name" placeholder="Name">
+                </div>
+                <div class="form-group">
+                    <label for="form-email">Email Address</label>
+                    <input type="email" class="form-control" id="form-email" placeholder="Email Address">
+                </div>
+                <div class="form-group">
+                    <label for="form-subject">Telephone</label>
+                    <input type="text" class="form-control" id="form-subject" placeholder="Subject">
+                </div>
+                <div class="form-group">
+                    <label for="form-message">Email your Message</label>
+                    <textarea class="form-control" id="form-message" placeholder="Message"></textarea>
+                </div>
+                <button class="btn btn-default" type="submit">Submit</button>                
+            </form> <br> <div class="col-md-6">
+            <div id="googlemap"></div>
+ <iframe class="size" Src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2940.4893172168076!2d55.40877841442182!3d25.28735368385434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1swhite+light+creative+advertising!5e1!3m2!1sen!2sae!4v1542110497323" width="400" height="320" frameborder="0" style="border:0" allowfullscreen></iframe>     
+        </div>
+        </div>
+        </div>
+    </div>
+</div>   <?php
+        // put your code here
+        ?>
+        <div id="footer"> &nbsp;&nbsp;For inquiry &nbsp;&nbsp;&nbsp; <img src="bb.jpg">info@momentuz.com &nbsp;&nbsp;&nbsp;&nbsp; <img src='aa.png'>042555654&nbsp;&nbsp;&nbsp;</div>
+    </body>
+</html>
